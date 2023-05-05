b17d46081d6882bfb2f1c98eda269d83244832ef
Samy Qais <samy.qais@orange.com>
2023-05-05 10:11:54 +0200

    Image Resizing OK

--- src/Service/ProfileService.php
+++ src/Service/ProfileService.php
@@ -5,11 +5,14 @@ namespace App\Service;
 use App\Dto\AvatarDto;
 use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
+use Intervention\Image\ImageManager;
 use RuntimeException;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ProfileService
 {
+    private const SIZE = 150;
+
     public function __construct(
         private readonly ValidatorInterface $validator,
         private readonly EntityManagerInterface $entityManager
@@ -18,16 +21,21 @@ class ProfileService
 
     public function updateAvatar(AvatarDto $data): void
     {
-        $errors = $this->validator->validate($data);
-        if ($errors->count() > 0) {
-            throw new RuntimeException($errors->get(0)->getMessage());
-        }
         if (false === $data->file->getRealPath()) {
             throw new RuntimeException('Impossible de redimensionner un avatar non existant');
         }
 
+        $manager = new ImageManager(['driver' => 'gd']);
+        $manager->make($data->file)->fit(self::SIZE, self::SIZE)->save($data->file->getRealPath());
+
         $data->user->setAvatarFile($data->file);
         $data->user->setUpdatedAt(new DateTimeImmutable());
+
+        $errors = $this->validator->validate($data);
+        if ($errors->count() > 0) {
+            throw new RuntimeException($errors->get(0)->getMessage());
+        }
+
         $this->entityManager->flush();
     }
 }
